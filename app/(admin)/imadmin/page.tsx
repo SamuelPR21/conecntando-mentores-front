47becbc3fe25fe75994b05adb71946eabd1bb897
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2024-05-21 14:09:06 -0500

    Started admin interface

---
+++ app/(admin)/imadmin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Trabajos from '@/components/shared/Trabajos'
+
+const AdminPage = () => {
+  return (
+    <div>
+        <Trabajos />
+    </div>
+  )
+}
+
+export default AdminPage;
